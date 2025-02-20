95ed40e36b93aa3a68e6b973fd0afae9371a4fc6
lrztnc <lorenzo.tincani@icloud.com>
2025-02-20 19:10:51 +0100

    Add Wave class to manage enemy waves

---
+++ Wave.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wave {
+    private List<Enemy> enemies;
+
+    public Wave(int numberOfEnemies) {
+        enemies = new ArrayList<>();
+        for (int i = 0; i < numberOfEnemies; i++) {
+            // enemies.add(/*aggiungere nemico bsae */))
+        }
+    }
+}
